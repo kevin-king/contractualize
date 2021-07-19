a70d28271e8ab9d5c9675ff5993719fa716008fe
Kevin King <kking@slalom.com>
2021-07-19 14:15:45 -0400

    Added Postman, Postman Extensions, and I prefix on TS type generation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs'
 import { Command, flags } from '@oclif/command'
 import j2s from 'joi-to-swagger'
 import { generateApi } from 'swagger-typescript-api'
+// import Converter from 'openapi-to-postmanv2'
+const Converter = require('openapi-to-postmanv2')
 
 function forEachFileIn(dirPath: string, callback: (arg0: string, arg1: string) => void) {
   fs.readdirSync(dirPath).forEach(file => {
@@ -15,8 +17,6 @@ function forEachFileIn(dirPath: string, callback: (arg0: string, arg1: string) =
   })
 }
 
-// con --path ./api-contracts
-// con --output ./api-contracts/autogen
 class ContractConverter extends Command {
   static description = 'describe the command here'
 
@@ -27,6 +27,8 @@ class ContractConverter extends Command {
     path: flags.string({char: 'p', description: 'Path to directory with Joi schemas'}),
     output: flags.string({char: 'o', description: 'Directory to store output'}),
     copy: flags.boolean({char: 'c', description: 'Copies Joi schemas to directory specified by output'}),
+    postman: flags.boolean({description: 'Compiles Postman scripts from OAS spec'}),
+    ts: flags.boolean({description: 'Compiles Typescript Interfaces from OAS spec'}),
   }
 
   // static args = [{name: 'file'}]
@@ -35,6 +37,7 @@ class ContractConverter extends Command {
     const {args, flags} = this.parse(ContractConverter)
 
     const INTPUT_PATH = path.resolve(process.cwd(), flags.path || '')
+    const TMP_PATH = `${INTPUT_PATH}/tmp` // TODO
 
     console.log('---------------')
     console.log(' Compiling Joi ')
@@ -49,6 +52,7 @@ class ContractConverter extends Command {
       const outputDir = `${dirPath.replace('/joi', '/autogen')}/`
       fs.mkdirSync(outputDir, {recursive: true})
       fs.writeFileSync(`${outputDir}/${file.replace('.js', '.json')}`, JSON.stringify(swagger))
+      fs.copyFileSync(`${dirPath}/${file}`, `${dirPath}/${file}`.replace('/joi', '/autogen').replace('.js', '.joi.js'))
     })
 
     console.log('---------------')
@@ -69,6 +73,17 @@ class ContractConverter extends Command {
 
       generateApi({
         input: path.resolve(process.cwd(), `${INTPUT_PATH}/autogen/${fileName}`),
+        hooks: {
+          // onCreateComponent: (component) => console.log(component),
+          // onCreateRequestParams: (rawType) => console.log(rawType),
+          // onCreateRoute: (routeData) => console.log(routeData),
+          // onCreateRouteName: (routeNameInfo, rawRouteInfo) => console.log(routeNameInfo),
+          // onFormatRouteName: (routeInfo, templateRouteName) => console.log(routeInfo),
+          onFormatTypeName: (typeName, rawTypeName) => `I${typeName}`,
+          // onInit: (configuration) => console.log(component),
+          // onParseSchema: (originalSchema, parsedSchema) => console.log(component),
+          // onPrepareConfig: (currentConfiguration) => console.log(component),
+        },
       })
       .then(({ files, configuration }) => {
         files.forEach(({ content, name }) => {
@@ -76,6 +91,43 @@ class ContractConverter extends Command {
         })
       })
       .catch(error => console.error(error))
+    })
+
+    console.log('-------------------')
+    console.log(' Compiling Postman ')
+    console.log('-------------------')
+    fs.readdirSync(`${INTPUT_PATH}/autogen/`).forEach((fileName: string) => {
+      if (fileName.indexOf('.oas.json') <= -1) return
+
+      const openapiData = fs.readFileSync(`${INTPUT_PATH}/autogen/${fileName}`, {encoding: 'UTF8'})
+
+      Converter.convert({ type: 'string', data: openapiData }, {}, (error: any, conversionResult: any) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+
+        if (conversionResult.result) {
+          fs.writeFileSync(`${INTPUT_PATH}/autogen/${fileName.replace('.oas.json', '.postman.json')}`, JSON.stringify(conversionResult.output[0].data))
+        } else {
+          console.error('Could not convert', conversionResult.reason)
+        }
+      })
+    })
+
+    console.log('----------------')
+    console.log(' Extend Postman ')
+    console.log('----------------')
+    forEachFileIn(`${INTPUT_PATH}/autogen/`, (dirPathAutogen, fileAutogen) => {
+      if (fileAutogen.indexOf('.postman.json') <= -1) return
+
+      forEachFileIn(`${INTPUT_PATH}/postman/`, (dirPathPostman, filePostman) => {
+
+        if (fileAutogen.replace('.postman.json', '.js') === filePostman) {
+          const postmanObject = require(`${dirPathPostman}${filePostman}`)
+          fs.writeFileSync(`${dirPathAutogen}${fileAutogen}`, JSON.stringify(postmanObject))
+        }
+      })
     })
   }
 }
