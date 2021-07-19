2080cee4d52a14904fa41322e6fb90dee04e01aa
Kevin King <kking@slalom.com>
2021-07-19 13:14:16 -0400

    Got TS compiling

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import {Command, flags} from '@oclif/command'
+import * as path from 'path'
+import * as fs from 'fs'
+import { Command, flags } from '@oclif/command'
 import j2s from 'joi-to-swagger'
-import {generateApi} from 'swagger-typescript-api';
+import { generateApi } from 'swagger-typescript-api'
 
 function forEachFileIn(dirPath: string, callback: (arg0: string, arg1: string) => void) {
-  fs.readdirSync(dirPath).forEach((file) => {
-    const absolutePath = path.join(dirPath, file);
+  fs.readdirSync(dirPath).forEach(file => {
+    const absolutePath = path.join(dirPath, file)
     if (fs.statSync(absolutePath).isDirectory()) {
-      forEachFileIn(absolutePath, callback);
+      forEachFileIn(absolutePath, callback)
     } else {
-      callback(dirPath, file);
+      callback(dirPath, file)
     }
-  });
+  })
 }
 
 // con --path ./api-contracts
@@ -40,53 +40,43 @@ class ContractConverter extends Command {
     console.log(' Compiling Joi ')
     console.log('---------------')
     forEachFileIn(`${INTPUT_PATH}/joi`, (dirPath: string, file: string) => {
-      const model = require(`${dirPath}/${file}`);
+      const model = require(`${dirPath}/${file}`)
       const joiSchema = model[file.replace('.js', '')]
 
-      if (!joiSchema) return;
-      const { swagger } = j2s(joiSchema, {});
+      if (!joiSchema) return
+      const { swagger } = j2s(joiSchema, {})
 
-      const outputDir = `${dirPath.replace('/joi', '/autogen')}/`;
+      const outputDir = `${dirPath.replace('/joi', '/autogen')}/`
       fs.mkdirSync(outputDir, {recursive: true})
-      fs.writeFile(`${outputDir}/${file.replace('.js', '.json')}`, JSON.stringify(swagger), function (err) {
-        if (err) console.log(err)
-      })
+      fs.writeFileSync(`${outputDir}/${file.replace('.js', '.json')}`, JSON.stringify(swagger))
     })
 
     console.log('---------------')
     console.log(' Compiling OAS ')
     console.log('---------------')
-    fs.readdir(INTPUT_PATH || '', (err, fileNames) => {
-      fileNames.forEach((fileName) => {
-        if (fileName.indexOf('.js') > -1) {
-          const swagger = require(`${INTPUT_PATH}/${fileName}`);
-          fs.writeFile(`${INTPUT_PATH}/autogen/${fileName.replace('.js', '.json')}`, JSON.stringify(swagger), function (err) {
-            if (err) console.log(err);
-          });
-        }
-      });
-    });
+    fs.readdirSync(INTPUT_PATH).forEach((fileName: string) => {
+      if (fileName.indexOf('.js') > -1) {
+        const swagger = require(`${INTPUT_PATH}/${fileName}`)
+        fs.writeFileSync(`${INTPUT_PATH}/autogen/${fileName.replace('.js', '.oas.json')}`, JSON.stringify(swagger))
+      }
+    })
 
-    // console.log('--------------')
-    // console.log(' Compiling TS ')
-    // console.log('--------------')
-    // forEachFileIn(`${INTPUT_PATH}/autogen`, (dirPath: string, file: string) => {
-    //   if (file.indexOf('.json') <= -1) return
-    //
-    //   /* NOTE: all fields are optional expect one of `output`, `url`, `spec` */
-    //   generateApi({
-    //     name: "MySuperbApi.ts",
-    //     output: path.resolve(process.cwd(), "./src/__generated__"),
-    //     url: 'http://api.com/swagger.json',
-    //     input: path.resolve(process.cwd(), './foo/swagger.json'),
-    //   })
-    //     .then(({ files, configuration }) => {
-    //       files.forEach(({ content, name }) => {
-    //         fs.writeFile(path, content);
-    //       });
-    //     })
-    //     .catch(e => console.error(e))
-    // })
+    console.log('--------------')
+    console.log(' Compiling TS ')
+    console.log('--------------')
+    fs.readdirSync(`${INTPUT_PATH}/autogen/`).forEach((fileName: string) => {
+      if (fileName.indexOf('.oas.json') <= -1) return
+
+      generateApi({
+        input: path.resolve(process.cwd(), `${INTPUT_PATH}/autogen/${fileName}`),
+      })
+      .then(({ files, configuration }) => {
+        files.forEach(({ content, name }) => {
+          fs.writeFileSync(`${INTPUT_PATH}/autogen/${fileName.replace('.oas.json', '.ts')}`, content)
+        })
+      })
+      .catch(error => console.error(error))
+    })
   }
 }
 
